74df43bf705cc2890e03aaf1bf3c9dec54da6474
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-07 16:21:31 -0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,25 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      primary: '#0252CD',
+      secondary: "#FFBE62",
+      black: {
+        200: '#151E2C',
+        300: '#192333',
+        400: '#778295'
+      },
+      white: {
+        900: '#FFFFFF',
+        800: '#F3F8FF',
+        500: '#6F74A7'
+      },
+      
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
