f1c7845c168f96e850630d29436ee2c315d33e60
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-09 08:57:22 -0500

    feat: mobile responsive Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
+import linkedIn from '../public/linkedIn.svg'
+import github from '../public/github.svg'
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <footer>
-        <div>
-            <div>
-                <p className='text-black-400 text-lg'>2023 Kamsiyonna. All rights reserved.</p>
+    <footer className="py-12 px-[47.5px] bg-white-800 sm:py-[52px] md:px-[85px]">
+        <div className="flex flex-col items-center w-full gap-4 sm:flex-row sm:justify-between">
+            <div className="flex">
+                <p className="text-black-400 items-center body-reg">&copy; 2023 Kamsiyonna. All rights reserved.</p>
             </div>
-            <div className='flex gap-6'>
-                <div className='w-6 h-6 border'></div>
-                <div className='w-6 h-6 border'></div>
+            <div className="flex gap-6 justify-center">
+                <Link href={"https://www.linkedin.com/in/kamsiyonna-obi-50a020167/"} className="w-6 h-6"><Image src={linkedIn} alt="linkedIn"/></Link>
+                <Link href={"https://github.com/KamsiyonnaObi"} className="w-6 h-6"><Image src={github} alt="github"/></Link>
             </div>
         </div>
     </footer>
