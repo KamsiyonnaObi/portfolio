4f1fdaf2f02bd60be4a6b3ed0a933392596d606e
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-11 04:08:47 -0500

    feat: create skills component

---
+++ components/Skills.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const Skills = (props: Props) => {
+  return (
+    <div className="flex">
+        <h1 className="inline-block highight text-[36px] font-bold leading-[41.4px] tracking-[0.36px] text-black-200 items-center">
+            My Skills
+        </h1>
+        <div></div>
+    </div>
+  )
+}
+
+export default Skills
