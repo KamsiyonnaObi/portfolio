9a515814cd26206bde1052359c3612156cf9322b
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-15 19:17:44 -0500

    feat: basic page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,32 @@ import Skills from '@/components/Skills';
 import {Button} from '@nextui-org/button'; 
 export default function Home() {
   return (
-    <main className="light bg-background flex min-h-screen px-6 flex-col items-center justify-between ">
+    <main className="flex flex-col min-h-screen p-0 m-0 items-center justify-between">
       <div className="flex flex-col">
-        <div className="bg-white-800 sm:mb-12">   
+        {/* Hero Section */}
+        <section className="bg-white-800 sm:pb-12 dark:bg-black-300">   
           <Hero />
-        </div>
-        <section className="py-12 bg-white-900">
+        </section>
+        {/* Skills Section */}
+        <section className="py-12 bg-white-900 dark:bg-black-200">
           <Skills />
+        </section>
+        {/* Services Section */}
+        <section className="bg-white-800 sm:pb-12 dark:bg-black-300">   
+          <div>What service do I provide</div>
+        </section>
+        {/* Work Experience */}
+        <section className="py-12 bg-white-900 dark:bg-black-200">
+          <div>Work Experience</div>
+          <div>Featured Projects</div>
+        </section>
+        {/* Testimonials */}
+        <section className="bg-white-800 sm:pb-12 dark:bg-black-300">   
+          <div>What they say about me</div>
+        </section>
+        {/* Contact Me */}
+        <section className="py-12 bg-white-900 dark:bg-black-200">
+          <div>Contact section</div>
         </section>
       </div>
     </main>
