21770fb792bc236c01e93633166bc30a87ac7589
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-16 15:57:05 -0500

    chore: include padding

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Hero from '@/components/Hero';
 import Skills from '@/components/Skills';
-import {Button} from '@nextui-org/button'; 
+import Services from '@/components/Services';
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen p-0 m-0 items-center justify-between">
@@ -15,8 +15,8 @@ export default function Home() {
           <Skills />
         </section>
         {/* Services Section */}
-        <section className="bg-white-800 sm:pb-12 dark:bg-black-300">   
-          <div>What service do I provide</div>
+        <section className="py-12 px-6 bg-white-800 sm:pb-12 dark:bg-black-300">   
+          <Services/>
         </section>
         {/* Work Experience */}
         <section className="py-12 bg-white-900 dark:bg-black-200">
