971b3e2d189759949de0a77134498e214eab7e94
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-16 15:57:42 -0500

    feat: create services section

---
+++ components/Services.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ServiceCards from './ServiceCards';
+
+type Props = {}
+
+const Services = (props: Props) => {
+  return (
+    <article className="flex flex-col px-[28.82px] mx-auto gap-10">
+        {/* Heading */}
+        <div className="flex">
+            <h1 
+                className="text-black-200 text-[36px] font-bold text-center sm:header2 dark:text-white-900"
+            >
+                What <span className="highlight">service</span> do I provide
+            </h1>
+        </div>
+        <div className="flex flex-col">
+            <ServiceCards />
+            
+        </div>
+    </article>
+  )
+}
+
+export default Services
