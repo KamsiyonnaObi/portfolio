235ba07e7d2f63670160a4a77e3a7e1f4b4163ab
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-25 09:48:41 -0500

    feat: Project card mobile version

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 type Props = {
@@ -18,15 +19,38 @@ const ProjectCard = (props: Props) => {
   return (
     <>
         {props.projects.data.map((project, idx) => (
-            <div key={idx} className={`flex bg-[${project.color}]`}>
-                <div>
-                    <h1 className="text-white-900 text-[32px] leading-[36.8px] tracking-[-0.36px] font-bold sm:header2">
-                        {`${project.title} - ${project.desc}`}
-                    </h1>
+            <div key={idx} className={`flex flex-col mx-auto justify-center bg-[${project.color}] gap-6 py-[46px]`}>
+                <div className="flex flex-col gap-[30px] px-6">
+                    <div>
+                        <h1 className="text-white-900 text-[32px] leading-[36.8px] tracking-[-0.36px] font-bold sm:header2">
+                            {`${project.title} - ${project.desc} `}
+                        </h1>
+                    </div>
+                    <div className="flex gap-5">
+                        <div>
+                            {project.frontEnd?.map((item, idx) => (
+                                <div key={idx} className="flex bg-white-900 bg-opacity-20">
+                                    {item}
+                                </div>
+                            ))}
+                        </div>
+                        <div>
+                            {project.backEnd?.map((item, idx) => (
+                                <div key={idx} className="flex bg-white-900 bg-opacity-20">
+                                    {item}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div>
+                        <Link href={"/"}>
+                            <p>See Project Details</p>
+                        </Link>
+                    </div>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className="w-[320px] pl-6">
+                    <div className="h-[155px] border"></div>
+                </div>
             </div>
         ))}
     </>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,15 +4,15 @@ import data from '../constants/projects.json';
 
 const Projects = () => {
   return (
-    <article className="flex flex-col justify-center gap-10">
+    <article className="flex flex-col px-6 justify-center gap-10">
     {/* Heading */}
         <div className="flex mx-auto">
             <h1 className="text-black-200 text-[36px] font-bold sm:header2 dark:text-white-900">
                 Featured <span className="highlight">Projects</span>
             </h1>
         </div>
-        {/* Projects Icons */}
-        <div className="flex sm:flex-wrap sm:justify-center sm:gap-[43.74px]">
+        {/* Project Cards */}
+        <div className="flex flex-col gap-9 sm:flex-wrap sm:justify-center sm:gap-[43.74px]">
             <ProjectCard 
             projects={data}
             />
