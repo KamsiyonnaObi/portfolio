70ae1b6516605f2a297f4816067ac47d5b15e1b7
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-25 10:30:53 -0500

    refactor: tech stack

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -19,7 +19,7 @@ const ProjectCard = (props: Props) => {
   return (
     <>
         {props.projects.data.map((project, idx) => (
-            <div key={idx} className={`flex flex-col mx-auto justify-center bg-[${project.color}] gap-6 py-[46px]`}>
+            <div key={idx} className={`flex flex-col w-fit h-fit mx-auto justify-center !bg-[${project.color}] gap-6 py-[46px] sm:flex-row sm:gap-[30.89px]`}>
                 <div className="flex flex-col gap-[30px] px-6">
                     <div>
                         <h1 className="text-white-900 text-[32px] leading-[36.8px] tracking-[-0.36px] font-bold sm:header2">
@@ -27,29 +27,21 @@ const ProjectCard = (props: Props) => {
                         </h1>
                     </div>
                     <div className="flex gap-5">
-                        <div>
-                            {project.frontEnd?.map((item, idx) => (
-                                <div key={idx} className="flex bg-white-900 bg-opacity-20">
-                                    {item}
-                                </div>
-                            ))}
+                        <div className="p-2.5 bg-white-900 bg-opacity-20 rounded-md">
+                            <p className="inline sm-reg">{project.frontEnd?.join(', ')}</p>
                         </div>
-                        <div>
-                            {project.backEnd?.map((item, idx) => (
-                                <div key={idx} className="flex bg-white-900 bg-opacity-20">
-                                    {item}
-                                </div>
-                            ))}
+                        <div className="p-2.5 bg-white-900 bg-opacity-20 rounded-md">
+                            <p className="inline sm-reg">{project.backEnd?.join(', ')}</p>
                         </div>
                     </div>
                     <div>
                         <Link href={"/"}>
-                            <p>See Project Details</p>
+                            <p className="body-bold">See Project Details</p>
                         </Link>
                     </div>
                 </div>
                 <div className="w-[320px] pl-6">
-                    <div className="h-[155px] border"></div>
+                    <div className="h-[155px] border sm:h-[331px]"></div>
                 </div>
             </div>
         ))}
