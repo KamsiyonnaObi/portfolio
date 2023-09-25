506e7fb55be4536fe6cd45e870efccda1b625634
Kamsiyonna <obikamsi@gmail.com>
2023-09-25 13:04:37 -0500

    Update components/ExperienceCard.tsx
    Co-authored-by: Brandon Etter <brandonetter@gmail.com>

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -18,7 +18,7 @@ const ExperienceCard = (props: Props) => {
   return (
     <>
         {props.experiences.data.map((exp, idx) => (
-            <Card key={idx} className="Card Card:hover w-[345px] py-[36px] pl-[36px] rounded-[10px] gap-[18px] sm:w-[607px] dark:bg-transparent hover:dark:bg-black-300">
+            <Card key={idx} className="Card w-[345px] py-[36px] pl-[36px] rounded-[10px] gap-[18px] sm:w-[607px] dark:bg-transparent hover:dark:bg-black-300">
                 <div className="sm:flex sm:gap-9">
                     <CardHeader className="flex p-0 w-auto">
                         <div className="h-[58px] w-[58px] rounded-[10px] border">
