bcacd5b530345fec6ac3cd1470590d81b47e0d44
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-30 11:02:36 -0500

    feat: project images

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react'
 
-import projects from '@/public/project-images/Morrent/Morrent-Laptop.png'
 type Props = {
     projects: { 
         data: 
@@ -31,16 +30,19 @@ const ProjectCard = (props: Props) => {
             return (
             <div key={idx} style={pcolor} className={`flex flex-col ${swap ? 'lg:flex-row-reverse' : ''} w-[345px] h-fit mx-auto justify-center rounded-[20px] gap-6 py-[46px] lg:w-fit lg:flex-row lg:gap-[30.89px]`}>
                 <div className="flex flex-col self-center content-center gap-[30px] px-6 lg:gap-6">
-                    <div>
+                    <div className="max-w-[452px]">
                         <h1 className="text-white-900 text-[32px] leading-[36.8px] tracking-[-0.36px] font-bold lg:header2">
-                            {`${project.title} - `}<br/>{` ${project.desc} `}
+                            {`${project.title} - `}
+                                <span className="inline-block lg:inline">
+                                    {` ${project.desc} `}
+                                </span>
                         </h1>
                     </div>
                     <div className="flex gap-5">
-                        <div className="p-2.5 bg-white-900 bg-opacity-20 rounded-md">
+                        <div className="sm-reg p-2.5 bg-white-900 bg-opacity-20 rounded-md">
                             <p className="inline lg-reg">{project.frontEnd?.join(', ')}</p>
                         </div>
-                        <div className="p-2.5 bg-white-900 bg-opacity-20 rounded-md">
+                        <div className="sm-reg p-2.5 bg-white-900 bg-opacity-20 rounded-md">
                             <p className="inline lg-reg">{project.backEnd?.join(', ')}</p>
                         </div>
                     </div>
@@ -50,9 +52,12 @@ const ProjectCard = (props: Props) => {
                         </Link>
                     </div>
                 </div>
-                <div className="w-[320px] pl-6 lg:w-[585px]">
-                    <div className="h-[155px] border lg:h-[331px]">
-                        <Image src={project.laptopImg} width={500} height={500} alt={project.imgAlt}/>
+                <div className="flex w-[320px] lg:w-[585px]">
+                    <div className="relative w-[270px] h-[155px] lg:w-[575.3px] lg:h-[331px] overflow-hidden">
+                        <Image src={project.laptopImg} className="object-contain" fill alt={project.imgAlt}/>
+                    </div>
+                    <div className="relative w-[66.74px] lg:w-[142.4px]">
+                        <Image src={project.mobileImg} className="object-contain" fill alt={project.imgAlt}/>
                     </div>
                 </div>
             </div>
