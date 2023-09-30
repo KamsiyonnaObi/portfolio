83f054f28746eb621e1eba0bd4967ba5b2d564c5
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-30 17:06:03 -0500

    bug: fix mobile hover

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,45 +1,67 @@
-"use client"
-import React from 'react'
-import Image from 'next/image';
-import jsIcon from "../public/skill-icons/javascript.png";
+"use client";
+import React from "react";
+import Image from "next/image";
+
 import gitIcon from "../public/skill-icons/git.png";
+import jsIcon from "../public/skill-icons/javascript.png";
 import tsIcon from "../public/skill-icons/typescript.png";
 import reduxIcon from "../public/skill-icons/redux.png";
 
-type Props = {}
+type Props = {};
 
 const Skills = (props: Props) => {
   return (
-   <article className="flex flex-col justify-center gap-10">
-    {/* Heading */}
-    <div className="flex mx-auto">
-      <h1 className="text-black-200 text-[36px] font-bold highlight sm:header2 dark:text-white-900">My Skills</h1>
-    </div>
-    {/* Skill Icons */}
-    <div className="flex justify-between sm:flex-wrap sm:justify-center sm:gap-[43.74px]">
-      <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 hover:shadow-lg hover:w-[116px] hover:h-[116px]">
-        <div className=" self-center">
-          <Image className="grayscale hover:grayscale-0" src={jsIcon} alt="javascript"/>
+    <article className="flex min-h-[150px] flex-col justify-center gap-10">
+      {/* Heading */}
+      <section className="flex flex-col mx-auto justify-start gap-y-10">
+        <div className="flex mx-auto ">
+          <h1 className="text-black-200 text-[36px] font-bold highlight sm:header2 dark:text-white-900">
+            My Skills
+          </h1>
         </div>
-      </div>
-      <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 hover:shadow-lg hover:w-[116px] hover:h-[116px]">
-        <div className=" self-center">
-          <Image className="grayscale hover:grayscale-0" src={reduxIcon} alt="redux"/>
-        </div>
-      </div>
-      <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 hover:shadow-lg hover:w-[116px] hover:h-[116px]">
-        <div className=" self-center">
-          <Image className="grayscale hover:grayscale-0" src={tsIcon} alt="typescript"/>
-        </div>
-      </div>
-      <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 hover:shadow-lg hover:w-[116px] hover:h-[116px]">
-        <div className=" self-center">
-          <Image className="grayscale hover:grayscale-0" src={gitIcon} alt="git"/>
-        </div>
-      </div>
-    </div>
-   </article>
-  )
-}
+        {/* Skill Icons */}
+        <div className="flex flex-wrap w-[348px] h-[120px] gap-10 justify-center sm:gap-[43.74px] sm:w-fit sm:h-fit">
+          <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg sm:hover:w-[116px] sm:hover:h-[116px]">
+            <div className=" self-center">
+              <Image
+                className="grayscale hover:grayscale-0"
+                src={jsIcon}
+                alt="javascript"
+              />
+            </div>
+          </div>
+          <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg sm:hover:w-[116px] sm:hover:h-[116px]">
+            <div className=" self-center">
+              <Image
+                className="grayscale hover:grayscale-0"
+                src={reduxIcon}
+                alt="redux"
+              />
+            </div>
+          </div>
+          <div className="flex p-2 rounded-full justify-center bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg sm:hover:w-[116px] sm:hover:h-[116px]">
+            <div className=" self-center">
+              <Image
+                className="grayscale hover:grayscale-0"
+                src={tsIcon}
+                alt="typescript"
+              />
+            </div>
+          </div>
 
-export default Skills
+          <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg sm:hover:w-[116px] sm:hover:h-[116px]">
+            <div className=" self-center">
+              <Image
+                className="grayscale hover:grayscale-0"
+                src={gitIcon}
+                alt="git"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
+  );
+};
+
+export default Skills;
