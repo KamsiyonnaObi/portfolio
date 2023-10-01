07d4ed93ad3d7278c5e35502ee359b2ad061f0c2
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-01 14:17:11 -0500

    refator: skills animation

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -21,38 +21,38 @@ const Skills = (props: Props) => {
         </div>
         {/* Skill Icons */}
         <div className="flex flex-wrap w-[348px] min-h-[120px] gap-10 justify-center sm:gap-[43.74px] sm:w-fit sm:h-fit">
-          <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg sm:hover:w-[116px] sm:hover:h-[116px]">
+          <div className="flex group skills p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg ">
             <div className=" self-center">
               <Image
-                className="grayscale hover:grayscale-0"
+                className="grayscale group-hover:grayscale-0"
                 src={jsIcon}
                 alt="javascript"
               />
             </div>
           </div>
-          <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg sm:hover:w-[116px] sm:hover:h-[116px]">
+          <div className="flex group skills p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg">
             <div className=" self-center">
               <Image
-                className="grayscale hover:grayscale-0"
+                className="grayscale group-hover:grayscale-0"
                 src={reduxIcon}
                 alt="redux"
               />
             </div>
           </div>
-          <div className="flex p-2 rounded-full justify-center bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg sm:hover:w-[116px] sm:hover:h-[116px]">
+          <div className="flex group skills p-2 rounded-full justify-center bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg">
             <div className=" self-center">
               <Image
-                className="grayscale hover:grayscale-0"
+                className="grayscale group-hover:grayscale-0"
                 src={tsIcon}
                 alt="typescript"
               />
             </div>
           </div>
 
-          <div className="flex p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg sm:hover:w-[116px] sm:hover:h-[116px]">
+          <div className="flex group skills p-2 rounded-full bg-white-800 w-[52.77px] h-[52.77px] sm:w-[100px] sm:h-[100px] sm:p-4 dark:bg-black-300 sm:hover:shadow-lg">
             <div className=" self-center">
               <Image
-                className="grayscale hover:grayscale-0"
+                className="grayscale group-hover:grayscale-0"
                 src={gitIcon}
                 alt="git"
               />
