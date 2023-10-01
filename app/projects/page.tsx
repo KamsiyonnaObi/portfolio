6e76de04d23349c393518e1e2167a95f8adef38e
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-01 10:07:52 -0500

    chore: refactor case studies

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,12 +4,12 @@ const CaseStudies = () => {
   return (
     <>
       <section className="px-6 bg-white-800 sm:py-[72px] lg:px-12 xl:px-[85px] dark:bg-black-300">
-        <article className="flex min-h-[186px] flex-col justify-center gap-10">
+        <article className="flex min-h-[186px] py-12 flex-col justify-center gap-10">
           {/* Header */}
           <section className="flex flex-col mx-auto items-center gap-y-5">
             <div className="flex mx-auto ">
-              <h1 className="text-black-200 text-[42px] leading-[48.3px] font-bold sm:text-[64px] sm:leading-[83.2px] dark:text-white-900 ">
-                Recent <span className="highlight">Case Studies</span>
+              <h1 className="text-black-200 text-[42px] text-center leading-[48.3px] font-bold sm:text-[64px] sm:leading-[83.2px] dark:text-white-900 ">
+                Recent <span className="w-fit highlight">Case Studies</span>
               </h1>
             </div>
 
