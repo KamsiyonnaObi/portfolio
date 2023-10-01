c1d64bbb4c764dea0149dcd67dd7d4bd4aab0e58
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-09-30 19:25:49 -0500

    feat: include transitions

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,48 +1,59 @@
-"use client"
-import React from 'react';
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
-import { DigitalIcon, SlackIcon, FirefoxIcon, MailchimpIcon } from './svg/workIcons';
+"use client";
+import React from "react";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+
+import {
+  DigitalIcon,
+  SlackIcon,
+  FirefoxIcon,
+  MailchimpIcon,
+} from "./svg/workIcons";
 
 type Props = {
-    experiences: { 
-        data: 
-        { 
-            img: string; 
-            employer: string; 
-            job_title: string; 
-            years_worked: string;
-        }[];
-    };
-}
+  experiences: {
+    data: {
+      img: string;
+      employer: string;
+      job_title: string;
+      years_worked: string;
+    }[];
+  };
+};
 
 const ExperienceCard = (props: Props) => {
-    const svgComponent = [SlackIcon, FirefoxIcon, DigitalIcon, MailchimpIcon]
+  const svgComponent = [SlackIcon, FirefoxIcon, DigitalIcon, MailchimpIcon];
   return (
     <>
-        {props.experiences.data.map((exp, idx) => {
-            const IconComponent = svgComponent[idx];
-            return (
-            <Card key={idx} className="Card group w-[345px] py-[36px] pl-[36px] rounded-[10px] gap-[18px] sm:w-[607px] dark:bg-transparent hover:dark:bg-black-300">
-                <div className="sm:flex sm:gap-9">
-                    <CardHeader className="flex p-0 w-auto">
-                        <div className="flex h-[58px] w-[58px] rounded-[10px]">
-                            <IconComponent />
-                        </div>
-                    </CardHeader>
-                    
-                    <CardBody className="p-0">
-                        <div className="flex flex-col gap-[9px]">
-                            <h3 className="base-bold text-black-200 dark:text-white-900">{exp.employer}</h3>
-                            <p className="sm-reg text-white-500 dark:text-white-800">
-                                {`${exp.job_title} - ${exp.years_worked} Years Experience`}
-                            </p>
-                        </div>
-                    </CardBody>
+      {props.experiences.data.map((exp, idx) => {
+        const IconComponent = svgComponent[idx];
+        return (
+          <Card
+            key={idx}
+            className="Card group w-[345px] py-[36px] pl-[36px] rounded-[10px] gap-[18px] sm:w-[607px] dark:bg-transparent hover:dark:bg-black-300"
+          >
+            <div className="sm:flex sm:gap-9">
+              <CardHeader className="flex p-0 w-auto">
+                <div className="flex h-[58px] w-[58px] rounded-[10px]">
+                  <IconComponent />
                 </div>
-            </Card>
-        )})}
-    </>
-  )
-}
+              </CardHeader>
 
-export default ExperienceCard
+              <CardBody className="p-0">
+                <div className="flex flex-col gap-[9px]">
+                  <h3 className="base-bold text-black-200 dark:text-white-900">
+                    {exp.employer}
+                  </h3>
+                  <p className="sm-reg text-white-500 dark:text-white-800">
+                    {`${exp.job_title} - ${exp.years_worked} Years Experience`}
+                  </p>
+                </div>
+              </CardBody>
+            </div>
+          </Card>
+        );
+      })}
+    </>
+  );
+};
+
+export default ExperienceCard;
