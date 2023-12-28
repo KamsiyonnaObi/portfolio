4bea7c0f0a799cb14b9144f2e953d52b938c8d3c
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-12-27 21:28:37 -0600

    connect sanity

---
+++ schemas/project.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  name: "project",
+  type: "document",
+  title: "Projects",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name",
+    },
+  ],
+};
