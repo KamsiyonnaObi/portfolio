f4b2a8ef4294597e20fa2fd5eaf5120903ae079f
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-12-31 22:04:35 -0600

    chore: include icons for profile details

---
+++ components/svg/Arrow.tsx
@@ -0,0 +1,11 @@
+export const Arrow = () => (
+  <svg className="w-6 h-6 stroke-Accent-light dark:stroke-Accent-dark">
+    <path
+      d="M16.6663 8.2666L20.3996 11.9999M20.3996 11.9999L16.6663 15.7333M20.3996 11.9999H3.59961"
+      stroke="#0252CD"
+      strokeWidth="1.67"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
--- components/svg/index.ts
+++ components/svg/index.ts
@@ -1,2 +1,5 @@
 export { EmailIcon } from "./Email";
 export { PhoneIcon } from "./Phone";
+export { GitHub } from "./GitHub";
+export { Earth } from "./Earth";
+export { Arrow } from "./Arrow";
