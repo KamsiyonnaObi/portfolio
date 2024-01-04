5d83c49a1549b615ecbab127276d06ed19bbdce0
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-04 08:04:45 -0600

    feat: create info section Component

--- app/(main)/projects/[slug]/page.tsx
+++ app/(main)/projects/[slug]/page.tsx
@@ -5,9 +5,9 @@ import { SanityDocument } from "next-sanity";
 
 import { loadQuery } from "@/.sanity/lib/store";
 import { POST_QUERY } from "@/.sanity/lib/queries";
-import { Earth, GitHub, Arrow } from "@/components/svg";
 import { urlFor } from "@/utils/utils";
 import Header from "@/components/ProjectDetails/Header";
+import InfoSection from "@/components/ProjectDetails/InfoSection";
 
 import Error from "../error";
 
@@ -35,65 +35,15 @@ const ProjectDetails = async ({ params }: { params: { slug: string } }) => {
           repo={projectData.data[0].github}
         />
       </section>
+
       {/* Role & Tech Stack Section */}
       <section className="px-6 py-10 bg-white-900 lg:px-12 xl:px-[85px] sm:py-[72px] dark:bg-black-200 w-full">
-        <section className="flex flex-col gap-9 mb-10 w-fit lg:flex-row lg:justify-between lg:w-full lg:max-w-[880px] lg:mx-auto">
-          <div className="flex flex-col gap-2.5">
-            <h1 className="sm-bold text-black-400 dark:text-white-500 lg:body-bold">
-              {" "}
-              My Role
-            </h1>
-            <p className="paragraph-bold text-black-200 dark:text-white-900 lg:base-bold">
-              {projectData.data[0].role}
-            </p>
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <h1 className="sm-bold text-black-400 dark:text-white-500 lg:body-bold">
-              Start Date
-            </h1>
-            <p className="paragraph-bold text-black-200 dark:text-white-900 lg:base-bold">
-              {moment(projectData.data[0].startDate).format("DD/MM/YYYY")}
-            </p>
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <h1 className="sm-bold text-black-400 dark:text-white-500 lg:body-bold">
-              End Date
-            </h1>
-            <p className="paragraph-bold text-black-200 dark:text-white-900 lg:base-bold">
-              {moment(projectData.data[0].endDate).format("DD/MM/YYYY")}
-            </p>
-          </div>
-        </section>
-        <section className="flex flex-col gap-6 mt-[42px] lg:w-full lg:gap-11 lg:max-w-[880px] lg:mx-auto lg:mt-[72px]">
-          <div className="flex flex-col gap-[9px]">
-            <p className="caption-bold text-Accent-light dark:text-Accent-dark lg:sm-bold">
-              Technologies Used
-            </p>
-            <h1 className="heading3 text-black-200 dark:text-white-900 lg:header3">
-              Tech Stack
-            </h1>
-          </div>
-          <div className="flex flex-wrap gap-x-5 gap-y-[33px] lg:gap-x-9">
-            {projectData.data[0]?.stack?.length > 0 &&
-              projectData.data[0].stack.map((tech: any) => {
-                return (
-                  <div
-                    key={tech}
-                    className="flex items-center justify-center group skills p-2 rounded-full bg-white-800 w-[50px] h-[50px] lg:w-[93px] lg:h-[93px] lg:p-4 dark:bg-black-300 sm:hover:shadow-lg "
-                  >
-                    <div className="flex relative w-[25px] h-[25px] lg:w-[50px] lg:h-[50px] items-center justify-center">
-                      <Image
-                        className=""
-                        src={urlFor(tech.asset._ref).url()}
-                        alt={tech.caption}
-                        fill
-                      />
-                    </div>
-                  </div>
-                );
-              })}
-          </div>
-        </section>
+        <InfoSection
+          role={projectData.data[0].role}
+          startDate={projectData.data[0].startDate}
+          endDate={projectData.data[0].endDate}
+          techStack={projectData.data[0]?.stack}
+        />
       </section>
       {/* Services Section */}
       <section className="px-6 py-12 bg-white-900 sm:bg-white-800 lg:px-12 xl:px-[85px] sm:py-[72px] dark:bg-black-300"></section>
