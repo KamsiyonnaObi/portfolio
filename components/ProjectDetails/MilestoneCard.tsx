96b7dcd9e941e8ae6e6e47649ecf3577f78fdab7
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-05 13:42:05 -0600

    bug: fix icon size

--- components/ProjectDetails/MilestoneCard.tsx
+++ components/ProjectDetails/MilestoneCard.tsx
@@ -10,16 +10,16 @@ const MilestoneCard = ({
 }) => {
   return (
     <>
-      <div className="flex gap-2.5 items-start ">
+      <div className="flex gap-2.5">
         <div className="flex w-5 h-5 relative">
           <Image
-            className="mt-1.5"
+            className="mt-[3px] lg:mt-1.5"
             src={`${isLearning ? "/tick.png" : "/challenge.png"}`}
             alt="bullet-list"
             fill
           />
         </div>
-        <div className="flex items-start">
+        <div className="flex items-start w-fit">
           <p className="sm-reg text-white-500 dark:text-white-800 lg:body-reg">
             {point}
           </p>
