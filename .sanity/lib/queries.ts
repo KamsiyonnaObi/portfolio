ecc5a2820362d1eda370a6ac0e216c7e8515ea33
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-07 09:46:28 -0600

    chore: query for featured projects

--- .sanity/lib/queries.ts
+++ .sanity/lib/queries.ts
@@ -4,5 +4,5 @@ import { groq } from "next-sanity";
 
 export const GET_ALL = groq`*[]`;
 export const POSTS_QUERY = groq`*[_type == "project"]`;
-
+export const FEATURED_QUERY = groq`*[_type == "project" && isFeatured == true]{title, desc, slug, laptopImg, mobileImg, frontEndtags, backEndtags, color}`;
 export const POST_QUERY = groq`*[_type == "project" && slug.current == $slug]`;
