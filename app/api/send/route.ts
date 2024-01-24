b620d3ef37b7f195b292dbf12d41553965bdfec0
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-24 12:00:53 -0600

    chore: install and creare resend route

---
+++ app/api/send/route.ts
@@ -0,0 +1,24 @@
+import { Resend } from "resend";
+import { ReactElement } from "react";
+
+import { EmailTemplate } from "@/components/Contact/EmailTemplate";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { error, data } = await resend.emails.send({
+      from: "Kamsiyonna Portfolio <onboarding@resend.dev>",
+      to: ["obikamsi@gmail.com"],
+      subject: "Contact From Portfolio Site",
+      react: EmailTemplate({ firstName: "Jon Snow" }) as ReactElement,
+    });
+    if (error) {
+      return Response.json({ error });
+    }
+
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
