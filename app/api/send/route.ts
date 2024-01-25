38509d73a5b1fda7c67dba8e295d9fdb2f5eb26f
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-01-25 07:47:37 -0600

    chore: prepare route and email template to receive POST req

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -8,10 +8,15 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const { error, data } = await resend.emails.send({
-      from: "Kamsiyonna Portfolio <onboarding@resend.dev>",
+      from: "Portfolio <onboarding@resend.dev>",
       to: ["obikamsi@gmail.com"],
       subject: "Contact From Portfolio Site",
-      react: EmailTemplate({ firstName: "Jon Snow" }) as ReactElement,
+      react: EmailTemplate({
+        firstName: "Jon Snow",
+        message: "Winter is coming",
+        bestContact: "2043965056",
+        email: "headcrow@housestark.net",
+      }) as ReactElement,
     });
     if (error) {
       return Response.json({ error });
--- components/Contact/EmailTemplate.tsx
+++ components/Contact/EmailTemplate.tsx
@@ -2,12 +2,25 @@ import * as React from "react";
 
 interface EmailTemplateProps {
   firstName: string;
+  message: string;
+  email: string;
+  bestContact?: string;
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName,
+  message,
+  email,
+  bestContact,
 }) => (
   <div>
-    <h1>Welcome, {firstName}!</h1>
+    <h1>Hello Kamsiyonna!</h1>
+    <p>{firstName} has contacted you on your portfolio site.</p>
+    <p>Here&apos;s their message</p>
+    <i>{message}</i>
+    <p>Here is their email: {email}</p>
+    {firstName && (
+      <p>However, they would prefer if you reach out via {bestContact}</p>
+    )}
   </div>
 );
